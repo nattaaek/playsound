c205abfce1f1153e0564038ddc0f4746c0807e8a
nattaaek <changeWorld555+>
2018-10-06 16:28:40 +0700

    detect sound

--- playsound/chapter1/Chapter1_53.swift
+++ playsound/chapter1/Chapter1_53.swift
@@ -14,10 +14,6 @@ class Chapter1_53: UIViewController, AVAudioPlayerDelegate {
     var conversationSound: AVAudioPlayer = AVAudioPlayer()
     var cntSong = 0
     
-    let mic = AKMicrophone()
-    
-    var tracker: AKFrequencyTracker = AKFrequencyTracker()
-    
     @IBAction func nextPage(_ sender: Any) {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "chapter1_52")
         self.present(vc!, animated: true, completion: nil)
@@ -38,7 +34,7 @@ class Chapter1_53: UIViewController, AVAudioPlayerDelegate {
     }
     
     func songPlay() {
-        
+
         let path = Bundle.main.path(forResource: "p42.mp3", ofType: nil)!
         let url = URL(fileURLWithPath: path)
         do {
@@ -51,29 +47,41 @@ class Chapter1_53: UIViewController, AVAudioPlayerDelegate {
     }
     
     var timer: Timer!
-    var silence = AKBooster()
-    var frequencyCnt = 0;
-    func audioPlayerDidFinishPlaying( _ player: AVAudioPlayer, successfully flag: Bool) {
-        
-        
+    var frequencyCnt = 0
+    var mic = AKMicrophone()
+    var tracker = AKFrequencyTracker()
 
-        cntSong += 1;
-        if cntSong == 1 {
-            songPlay()
-            timer = Timer.scheduledTimer(withTimeInterval: 0.9, repeats: true, block: { _ in
-                if self.frequencyCnt >= 8{
-                    let mic = AKMicrophoneTracker()
-                    mic.start()
-                    print(mic.frequency)
-                }
-                self.frequencyCnt += 1
-            })
+    
+
+    func audioPlayerDidFinishPlaying( _ player: AVAudioPlayer, successfully flag: Bool) {
+        songPlay()
+        tracker = AKFrequencyTracker(mic)
+        let silence = AKBooster(tracker, gain: 0)
+        AudioKit.output = silence
+        try! AudioKit.start()
+        Timer.scheduledTimer( timeInterval: 0.9, target: self, selector: #selector(Chapter1_53.outputFrequency), userInfo: nil, repeats: true)
+
+
+    }
+    var cCount = 0
+    var dCount = 0
+    @objc func outputFrequency() {
+        frequencyCnt += 1
+        if frequencyCnt >= 8 {
+            if tracker.frequency == 261.6256 {
+                print("pass")
+            }
+            else {
+                print(tracker.frequency)
+            }
         }
+ 
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         btnNext.isHidden = true
-
+        
+        audioPlay()
     }
 }
