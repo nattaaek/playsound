1e68a69e76fece857e87b67ea36d93c486b3761e
rick-ihma <aaa88456@hotmail.com>
2019-05-05 22:00:59 +0800

    update chapter1_60

---
+++ Pods/Target Support Files/Pods-playsound/Pods-playsound-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_playsound : NSObject
+@end
+@implementation PodsDummy_Pods_playsound
+@end
---
+++ Pods/Target Support Files/Pods-playsoundUITests/Pods-playsoundUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_playsoundUITests : NSObject
+@end
+@implementation PodsDummy_Pods_playsoundUITests
+@end
