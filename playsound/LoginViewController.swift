14a260d701453a6fd27dc43c8bc425c1204db2e9
Nattaaek <w.nattaaek@yahoo.com>
2019-05-17 21:43:42 +0800

    connect to endpoint

--- playsound/LoginViewController.swift
+++ playsound/LoginViewController.swift
@@ -77,7 +77,44 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func doLogin(_ sender: Any) {
-
+        
+        let parameters = ["username": username.text, "password": password.text]
+        
+        let url = URL(string: "http://35.198.254.22:3000/member/login")!
+        
+        let session = URLSession.shared
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
     }
     
     @objc func dismissKeyboard() {
